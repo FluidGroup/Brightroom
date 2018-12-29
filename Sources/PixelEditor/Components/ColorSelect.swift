13421449bb074d2ae525b805c73e00c63cbe232f
Dani Lazarov <danny.lazarow@gmail.com>
2018-12-29 09:50:34 +0200

    color select - use separate CAShapeLayer

--- Sources/PixelEditor/Components/ColorSelect.swift
+++ Sources/PixelEditor/Components/ColorSelect.swift
@@ -96,76 +96,169 @@ public class ColorSelectButtons : UIControl {
   
 }
 
-public class ColorSelectItem: UIButton {
-  var color: UIColor? = nil {
+class CoreCircle: CAShapeLayer {
+  var color: CGColor? = nil {
     didSet {
       setNeedsDisplay()
     }
   }
   
-  override public func draw(_ rect: CGRect) {
-    
-    guard let context = UIGraphicsGetCurrentContext() else {
-      return
+  var progress: CGFloat = 1 {
+    didSet {
+      setNeedsDisplay()
     }
-    
-    let scale = contentScaleFactor
-    
-    guard
-      let layer = CGLayer(context, size: CGSize(width: bounds.size.width * scale, height: bounds.size.height * scale), auxiliaryInfo: nil),
-      let layerContext = layer.context
-      else {
-        return
-    }
-    
-    UIGraphicsPushContext(layerContext)
-    
-    layerContext.scaleBy(x: scale, y: scale)
-    
-    if let color = self.color {
-      let displayColor = color != .clear ? color : .black
-      displayColor.setStroke()
-      displayColor.setFill()
-    }
-    
-    line: do {
-      if isSelected {
-        let path = UIBezierPath(ovalIn: .init(origin: .init(x: bounds.midX - 10 + 3, y: bounds.midY - 10 + 3), size: .init(width: 20, height: 20)))
-        path.lineWidth = 2
-        path.stroke()
-      }
-    }
-    
-    dot: do {
-      if isSelected {
-        let path = UIBezierPath(ovalIn: .init(origin: .init(x: bounds.midX, y: bounds.midY), size: .init(width: 6, height: 6)))
-        path.fill()
-      }
+  }
+  
+  override init() {
+    super.init()
+    contentsScale = UIScreen.main.scale
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func progressInterpolated() -> CGFloat {
+    return max(min(progress, 1), 0) + 1
+  }
+  
+  override func draw(in context: CGContext) {
+    if let color = color {
+      context.setStrokeColor(color)
+      context.setFillColor(color)
     }
 
-    circle: do {
-      if !isSelected {
-        let path = UIBezierPath(ovalIn: .init(origin: .init(x: bounds.midX - 10 + 4, y: bounds.midY - 10 + 4), size: .init(width: 18, height: 18)))
-        path.fill()
-      }
-    }
-    
-    none: do {
-      if !isSelected, color == .clear {
-        UIColor.white.setStroke()
-        UIColor.white.setFill()
-        
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: bounds.midX - 10 + 5, y: bounds.midY + 10))
-        path.addLine(to: CGPoint(x: bounds.midX + 10, y: bounds.midY - 10 + 5))
-        path.lineWidth = 2
-        path.stroke()
-      }
-    }
-    
-    UIGraphicsPopContext()
-    
-    context.draw(layer, in: bounds)
+    let newSize = CGSize(width: bounds.width / 2.5 * progressInterpolated(), height: bounds.height / 2.5 * progressInterpolated())
+    let newPosition = CGPoint(x: bounds.midX - newSize.width/2, y: bounds.midY - newSize.height/2)
+    let path = UIBezierPath(ovalIn: CGRect(origin: newPosition, size: newSize))
+    context.addPath(path.cgPath)
+    context.fillPath()
   }
 }
 
+class BorderCircle: CAShapeLayer {
+  var color: CGColor? = nil {
+    didSet {
+      setNeedsDisplay()
+    }
+  }
+  
+  var progress: CGFloat = 1 {
+    didSet {
+      setNeedsDisplay()
+    }
+  }
+  
+  override init() {
+    super.init()
+    contentsScale = UIScreen.main.scale
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func progressInterpolated() -> CGFloat {
+    return max(min(progress, 1), 0) + 1
+  }
+  
+  override func draw(in context: CGContext) {
+    if let color = self.color {
+      context.setStrokeColor(color)
+      context.setFillColor(color)
+    }
+
+    let newSize = CGSize(width: bounds.width * progressInterpolated() - 2, height: bounds.height * progressInterpolated() - 2)
+    let newPosition = CGPoint(x: bounds.midX - newSize.width/2, y: bounds.midY - newSize.height/2)
+    let path = UIBezierPath(ovalIn: CGRect(origin: newPosition, size: newSize))
+    path.lineWidth = 2
+    
+    context.addPath(path.cgPath)
+    context.strokePath()
+  }
+}
+
+class StrikeThrough: CAShapeLayer {
+  
+  override init() {
+    super.init()
+    contentsScale = UIScreen.main.scale
+    fillRule = .evenOdd
+    fillColor = UIColor.black.cgColor
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func draw(in context: CGContext) {
+    let rectanglePath = UIBezierPath()
+    rectanglePath.move(to: CGPoint(x: 0, y: bounds.height))
+    rectanglePath.addLine(to: CGPoint(x: 0, y: bounds.height + 2))
+    rectanglePath.addLine(to: CGPoint(x: bounds.width - 2, y: 0))
+    rectanglePath.addLine(to: CGPoint(x: bounds.width - 3, y: 0))
+    rectanglePath.addLine(to: CGPoint(x: 0, y: bounds.height))
+    rectanglePath.close()
+    
+    let path = CGMutablePath()
+    path.addPath(rectanglePath.cgPath)
+    path.addRect(CGRect(x: 0, y: 0, width: 20, height: 20))
+    
+    self.path = path
+  }
+}
+
+public class ColorSelectItem: UIButton {
+  var color: UIColor? = nil {
+    didSet {
+      coreCircleLayer.color = color != .clear ? color?.cgColor : UIColor.black.cgColor
+      borderCircleLayer.color = color != .clear ? color?.cgColor : UIColor.black.cgColor
+    }
+  }
+  
+  public override var isSelected: Bool {
+    didSet {
+      coreCircleLayer.progress = !isSelected ? 0 : 1
+      borderCircleLayer.progress = !isSelected ? 0 : 1
+      coreCircleLayer.mask = isSelected && color == .clear ? strikeThroughLayer : nil
+    }
+  }
+  
+  public override init(frame: CGRect) {
+    super.init(frame: .zero)
+    setup()
+  }
+  
+  public required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  public override func awakeFromNib() {
+    super.awakeFromNib()
+    setup()
+  }
+  
+  var coreCircleLayer: CoreCircle!
+  var borderCircleLayer: BorderCircle!
+  var strikeThroughLayer: StrikeThrough!
+  
+  func setup() {
+    coreCircleLayer = CoreCircle()
+    borderCircleLayer = BorderCircle()
+    strikeThroughLayer = StrikeThrough()
+    
+    layer.addSublayer(coreCircleLayer)
+    layer.addSublayer(borderCircleLayer)
+    coreCircleLayer.mask = strikeThroughLayer
+  }
+  
+  public override func layoutSubviews() {
+    let newSize = CGSize(width: 20, height: 20)
+    let newPosition = CGPoint(x: bounds.width/2 - newSize.width/2, y: center.y - newSize.height/2)
+    
+    borderCircleLayer.frame = CGRect(origin: newPosition, size: newSize)
+    coreCircleLayer.frame = CGRect(origin: newPosition, size: newSize)
+    strikeThroughLayer.frame = coreCircleLayer.bounds
+    strikeThroughLayer.setNeedsDisplay()
+  }
+}
