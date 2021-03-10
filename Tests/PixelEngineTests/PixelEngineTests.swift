0bd78ea80908fa393afbf44aa3d49b4818af22fb
Muukii <muukii.app@gmail.com>
2021-03-10 19:44:31 +0900

    Add TestScheme

---
+++ Tests/PixelEngineTests/PixelEngineTests.swift
@@ -0,0 +1,30 @@
+//
+//  PixelEngineTests.swift
+//  PixelEngineTests
+//
+//  Created by Muukii on 2021/03/10.
+//  Copyright © 2021 muukii. All rights reserved.
+//
+
+import XCTest
+
+@testable import PixelEngine
+
+class PixelEngineTests: XCTestCase {
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    continueAfterFailure = false
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  }
+
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testHoge() {
+        
+  }
+}
