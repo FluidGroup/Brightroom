603722571d47280265892bb59bf2a7342f485ca0
Muukii <muukii.app@gmail.com>
2021-04-01 01:05:40 +0900

    Fix CGContext creation in PNG file

--- Sources/BrightroomEngine/Engine/CoreGraphics+.swift
+++ Sources/BrightroomEngine/Engine/CoreGraphics+.swift
@@ -31,17 +31,47 @@ extension CGContext {
   }
 
   static func makeContext(for image: CGImage, size: CGSize? = nil) throws -> CGContext {
-    let context = CGContext.init(
-      data: nil,
-      width: size.map { Int($0.width) } ?? image.width,
-      height: size.map { Int($0.height) } ?? image.height,
-      bitsPerComponent: image.bitsPerComponent,
-      bytesPerRow: 0,
-      space: try image.colorSpace.unwrap(),
-      bitmapInfo: image.bitmapInfo.rawValue
-    )
 
-    return try context.unwrap()
+    let context: CGContext
+    do {
+      context = try CGContext.init(
+        data: nil,
+        width: size.map { Int($0.width) } ?? image.width,
+        height: size.map { Int($0.height) } ?? image.height,
+        bitsPerComponent: image.bitsPerComponent,
+        bytesPerRow: 0,
+        space: try image.colorSpace.unwrap(),
+        bitmapInfo: image.bitmapInfo.rawValue
+      )
+      .unwrap()
+
+    } catch {
+
+      /**
+       First time creation of CGContext would fail if the image was created from iOS screenshot.
+       That would be Display P3 and 16bit.
+       
+       https://stackoverflow.com/a/42684334/2753383
+       */
+
+      var bitmapInfo = image.bitmapInfo
+
+      bitmapInfo.remove(.alphaInfoMask)
+      bitmapInfo.formUnion(.init(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue))
+
+      context = try CGContext.init(
+        data: nil,
+        width: size.map { Int($0.width) } ?? image.width,
+        height: size.map { Int($0.height) } ?? image.height,
+        bitsPerComponent: image.bitsPerComponent,
+        bytesPerRow: 0,
+        space: try image.colorSpace.unwrap(),
+        bitmapInfo: bitmapInfo.rawValue
+      )
+      .unwrap()
+    }
+
+    return context
   }
 
   fileprivate func detached(_ perform: () -> Void) {
