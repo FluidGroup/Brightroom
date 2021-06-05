bb07848e228d3ce7f27669d3838fec6b568d7eb4
Muukii <muukii.app@gmail.com>
2021-06-05 18:48:31 +0900

    Add failure test cases that create CGContext in the special images. #127

---
+++ Tests/BrightroomEngineTests/CGContextCreationTests.swift
@@ -0,0 +1,30 @@
+//
+//  CGContextCreationTests.swift
+//  BrightroomEngineTests
+//
+//  Created by Muukii on 2021/06/05.
+//  Copyright © 2021 muukii. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import BrightroomEngine
+
+final class CGContextCreationTests: XCTestCase {
+
+  func test_create_cgcontext() {
+
+    (1...13).forEach { i in
+      let imageName = "test-image-\(i)"
+      let cgImage = UIImage(named: imageName, in: _pixelengine_bundle, with: nil)!.cgImage!
+      do {
+        _ = try CGContext.makeContext(for: cgImage)
+      } catch {
+        print("❌", imageName, error.localizedDescription)
+        XCTFail(error.localizedDescription)
+      }
+    }
+
+  }
+}
