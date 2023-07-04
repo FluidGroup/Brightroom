52f97c56f06a8a0b2bc0d19867efe7690a57e06e
Antoine Marandon <antoine@marandon.fr>
2023-07-04 10:38:07 +0900

    Fix demo build

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
@@ -11,7 +11,7 @@ let package = Package(
     .library(name: "BrightroomUI", targets: ["BrightroomUI"]),
   ],
   dependencies: [
-    .package(name: "Verge", url: "https://github.com/VergeGroup/Verge.git", from: "11.0.0-beta.5"),
+    .package(url: "https://github.com/VergeGroup/Verge.git", branch: "main"),
     .package(url: "https://github.com/FluidGroup/TransitionPatch.git", from: "1.0.3")
   ],
   targets: [
@@ -19,7 +19,7 @@ let package = Package(
       name: "BrightroomEngine",
       dependencies: ["Verge"],
       exclude: ["Info.plist"]
-    ),      
+    ),
     .target(
       name: "BrightroomUI",
       dependencies: ["BrightroomEngine", "Verge", "TransitionPatch"],
