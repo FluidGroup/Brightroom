1105e3faf426306814bb26f15967085591f5628c
Hiroshi Kimura <muukii.app@gmail.com>
2024-02-22 17:50:52 +0900

    Update InsideOverlay support SwiftUI  (#223)

--- Sources/BrightroomUI/Shared/Components/Crop/CropView.CropInsideOverlay.swift
+++ Sources/BrightroomUI/Shared/Components/Crop/CropView.CropInsideOverlay.swift
@@ -131,13 +131,53 @@ extension CropView {
   @available(iOS 14, *)
   open class SwiftUICropInsideOverlay<Content: View>: CropInsideOverlayBase {
 
-    private let controller: UIHostingController<Content>
+    private let controller: UIHostingController<Container>
+    private let proxy: Proxy
 
-    public init(controller: UIHostingController<Content>) {
-      self.controller = controller
+    public init(@ViewBuilder content: @escaping (CropView.State.AdjustmentKind?) -> Content) {
+      
+      self.proxy = .init()
+      self.controller = .init(rootView: Container(proxy: proxy, content: content))
+
+      controller.view.backgroundColor = .clear
+      controller.view.preservesSuperviewLayoutMargins = false
+
       super.init()
       addSubview(controller.view)
       AutoLayoutTools.setEdge(controller.view, self)
+    }
+
+    open override func didBeginAdjustment(kind: CropView.State.AdjustmentKind) {
+      proxy.activeKind = kind
+    }
+
+    open override func didEndAdjustment(kind: CropView.State.AdjustmentKind) {
+      proxy.activeKind = nil
+    }
+
+    private final class Proxy: ObservableObject {
+
+      @Published var activeKind: CropView.State.AdjustmentKind?
+
+    }
+
+    private struct Container: View {
+
+      @ObservedObject var proxy: Proxy
+
+      private let content: (CropView.State.AdjustmentKind?) -> Content
+
+      public init(
+        proxy: Proxy,
+        content: @escaping (CropView.State.AdjustmentKind?) -> Content
+      ) {
+        self.content = content
+        self.proxy = proxy
+      }
+
+      var body: some View {
+        content(proxy.activeKind)
+      }
     }
 
   }
